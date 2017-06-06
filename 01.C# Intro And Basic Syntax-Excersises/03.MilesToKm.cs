e6329ce5587d27ce77f036db4cf6a710f0cebdd3
Tugay Emin <tgntrr@gmail.com>
2017-06-06 12:02:43 +0300

    update

---
+++ 01.C# Intro And Basic Syntax-Excersises/03.MilesToKm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class MilesToKm
+{
+    static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        double km = a * 1.60934;
+        Console.WriteLine("{0:F2}", km);
+
+    }
+}
+
